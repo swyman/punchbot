df9f32d6c830cd7024c4d6ecfd2ef5302b6ffe89
Sam Wyman <sam.wyman@gmail.com>
2014-05-09 15:08:13 -0700

    add thanks handling

--- lib/chatbot.rb
+++ lib/chatbot.rb
@@ -42,7 +42,7 @@ class Chatbot
       exec_command(match[1])
     elsif /^(hello|hi|greetings|sup|hey) punchbot/i =~ msg[:text]
       post_message("Hey #{msg[:name]}. How's it going?")
-    elsif /(thanks)[\s]?punchbot/i =~ msg[:text]
+    elsif /(thanks)[\s]?(punchbot|pb)/i =~ msg[:text]
       post_message("You're welcome, #{msg[:name]}.")
     end
   end
