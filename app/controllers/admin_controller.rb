d2c44446ee14fedccbf7884ffcd31fdec7f94d0e
swyman <sam.wyman@gmail.com>
2014-05-10 21:38:04 -0700

    add admin bot

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,31 @@
+class AdminController < ApplicationController
+  protect_from_forgery except: [:admin]
+
+  before_action :log_params
+  before_action :init_punchbot
+
+
+  # todo this doesn't work
+  def admin
+    if /(?:pb|punchbot sleep)/i =~ params[:pb_dadmin][:text]
+      $redis.set(sleep_key, 1)
+    elsif /((?:pb|punchbot) wakeup)/i =~ params[:pb_dadmin][:text]
+      $redis.del(sleep_key)
+    end
+  end
+
+  private
+
+  def init_punchbot
+    @bot = Chatbot.new
+  end
+
+  def log_params
+    puts params
+  end
+
+  def sleep_key
+    'admin-pb-sleep'
+  end
+
+end
